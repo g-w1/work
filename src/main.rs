b77dae01e5de2591e85a282b8ca9cc91f33d6ac8
g-w1 <jacoblevgw@gmail.com>
2020-07-18 21:02:18 -0400

    had something in main that I didn't wqant

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,24 @@ pub mod event;
 pub mod frontend;
 
 use database::*;
-use event::Event;
+// use event::Event;
 use rusqlite::{Connection, Result};
 
 fn main() -> Result<()> {
     let conn = Connection::open("./test.db")?;
-    // down(&conn)?;
     up(&conn)?;
-    let spanish_event = Event::new(String::from("spanish quiz"));
+    // let spanish_event = Event::new(String::from("spanish quiz"));
     // let other_event = Event::new(String::from("test2"));
     // other_event.into_database(&conn)?;
-    spanish_event.into_database(&conn)?;
-    update_event_by_id(
-        &conn,
-        Event {
-            id: Some(1),
-            summary: String::from("testing"),
-            done: true,
-        },
-    )?;
+    // spanish_event.into_database(&conn)?;
+    // update_event_by_id(
+    //     &conn,
+    //     Event {
+    //         id: Some(1),
+    //         summary: String::from("testing"),
+    //         done: true,
+    //     },
+    // )?;
     // let karate = Event::new(String::from("i need to practice karate"));
     // karate.into_database(&conn)?;
     config::parse(&conn)?;
